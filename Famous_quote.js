cf21f3b9e9bdc148e22778a3c4ce9dfda33c4b20
Shumaila Noreen <161513956+Sharniki098@users.noreply.github.com>
2024-05-20 01:08:49 -0700

    Add files via upload

---
+++ Famous_quote.js
@@ -0,0 +1,6 @@
+function printQuote() {
+    var quote = "try to be a rainbow in someone's cloud.";
+    var author = "Maya Angelou";
+    console.log("".concat(author, "once said, \"").concat(quote, "\""));
+}
+printQuote();
